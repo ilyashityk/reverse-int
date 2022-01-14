1ece463d2d1efda06fb755424967b38b3ae80908
ilyashityk <ilya350000@gmail.com>
2022-01-14 13:23:10 +0300

    feat: add function reverse into index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const absN = Math.abs(n);
+    const strN = absN.toString();
+    return strN[strN.length -1] + strN[strN.length -2] + strN[strN.length -3];
 }
